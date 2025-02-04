45345269a27e6b804df01629494035aa579900dd
chris <chris@legendserver.info>
2025-02-04 11:32:27 -0500

    Update MultiSelect Demo to ensure the disabled attribute works as expected

--- Components/src/MultiSelect/Demo.js
+++ Components/src/MultiSelect/Demo.js
@@ -3,6 +3,7 @@ import MultiSelect from './MultiSelect';
 
 export const Demo = () => {
   const [value1, setValue1] = useState(null);
+  const [value2, setValue2] = useState(null);
 
   return (
     <div id="app" style={{margin: '1rem'}}>
@@ -22,6 +23,25 @@ export const Demo = () => {
         onUpdate={(val) => setValue1(val)}
         placeholder="Star Trek TOS Crew"
         defaultValue={["mccoy"]}
+      />
+      <br/>
+      <br/>
+      Current Value: {JSON.stringify(value2)}
+      <br/>
+      <MultiSelect
+        options={[
+          {name: 'James T. Kirk', value: 'kirk'},
+          {name: 'Spock', value: 'spock'},
+          {name: 'Leonard McCoy', value: 'mccoy'},
+          {name: 'Nyota Uhura', value: 'uhura'},
+          {name: 'Montgomery Scott', value: 'scott'},
+          {name: 'Pavel Chekov', value: 'chekov'},
+          {name: 'Hikaru Sulu', value: 'sulu'},
+        ]}
+        onUpdate={(val) => setValue2(val)}
+        placeholder="Star Trek TOS Crew"
+        defaultValue={["spock"]}
+        disabled
       />
     </div>
   );
